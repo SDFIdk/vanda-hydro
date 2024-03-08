76e3683cac935677454aa1750ca847f53c05eb81
Mac Wendelboe <macwe@sdfi.dk>
2024-03-08 10:38:47 +0100

    Integration test water-levels

--- src/test/java/dk/dmp/vanda/hydro/httpjson/HydrometryServiceClientIT.java
+++ src/test/java/dk/dmp/vanda/hydro/httpjson/HydrometryServiceClientIT.java
@@ -1,6 +1,10 @@
 package dk.dmp.vanda.hydro.httpjson;
 
+import dk.dmp.vanda.hydro.HydrometryService;
 import dk.dmp.vanda.hydro.Station;
+import dk.dmp.vanda.hydro.WaterLevelMeasurement;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
@@ -8,43 +12,62 @@ import java.net.http.HttpClient;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HydrometryServiceClientIT {
+    static HttpClient http;
+    static HydrometryService service;
+    @BeforeAll
+    static void setup() {
+        URI endpoint = URI.create("https://vandah.test.miljoeportal.dk/api/");
+        http = HttpClient.newHttpClient();
+        service = new HydrometryServiceClient(new JsonStreamHttpClient(endpoint, http));
+    }
+
+    @AfterAll
+    static void teardown() throws Exception {
+        http.close();
+        ((HydrometryServiceClient)service).close();
+    }
+
     @Test
     void testStations() throws Exception {
-        URI endpoint = URI.create("https://vandah.test.miljoeportal.dk/api/");
-        try (HttpClient http = HttpClient.newHttpClient();
-             HydrometryServiceClient service = new HydrometryServiceClient(new JsonStreamHttpClient(endpoint, http))
-        ) {
-            Iterator<Station> iter = service.getStations().stationId("61000181").exec();
-            Station station = iter.next();
-            assertAll(
-                () -> assertFalse(iter.hasNext()),
-                () -> assertEquals("61000181", station.stationId()),
-                () -> assertEquals("Vandløb", station.locationType()),
-                () -> assertEquals(1, station.measurementPoints().size()),
-                () -> assertEquals(3, station.measurementPoints().getFirst().examinations().size()));
-        }
+        Iterator<Station> iter = service.getStations().stationId("61000181").exec();
+        Station station = iter.next();
+        assertAll(
+            () -> assertFalse(iter.hasNext()),
+            () -> assertEquals("61000181", station.stationId()),
+            () -> assertEquals("Vandløb", station.locationType()),
+            () -> assertEquals(1, station.measurementPoints().size()),
+            () -> assertEquals(3, station.measurementPoints().getFirst().examinations().size()));
     }
 
     @Test
     void testStationsAfter() throws Exception {
-        URI endpoint = URI.create("https://vandah.test.miljoeportal.dk/api/");
-        try (HttpClient http = HttpClient.newHttpClient();
-             HydrometryServiceClient service = new HydrometryServiceClient(new JsonStreamHttpClient(endpoint, http))
-        ) {
-            Iterator<Station> iter = service.getStations().stationId("61000181")
-                .withResultsAfter(OffsetDateTime.of(2018,1,1, 14,0,0,0, ZoneOffset.ofHours(1)))
-                .exec();
-            Station station = iter.next();
-            assertAll(
-                () -> assertFalse(iter.hasNext()),
-                () -> assertEquals("61000181", station.stationId()),
-                () -> assertEquals("Vandløb", station.locationType()),
-                () -> assertEquals(1, station.measurementPoints().size()),
-                () -> assertEquals(2, station.measurementPoints().getFirst().examinations().size()));
-        }
+        Iterator<Station> iter = service.getStations().stationId("61000181")
+            .withResultsAfter(OffsetDateTime.of(2018,1,1, 14,0,0,0, ZoneOffset.ofHours(1)))
+            .exec();
+        Station station = iter.next();
+        assertAll(
+            () -> assertFalse(iter.hasNext()),
+            () -> assertEquals("61000181", station.stationId()),
+            () -> assertEquals("Vandløb", station.locationType()),
+            () -> assertEquals(1, station.measurementPoints().size()),
+            () -> assertEquals(2, station.measurementPoints().getFirst().examinations().size()));
+    }
+
+    @Test
+    void testWaterLevels() throws Exception {
+        Iterator<WaterLevelMeasurement> iter = service.getWaterLevels().stationId("61000181")
+            .from(OffsetDateTime.parse("2023-10-02T19:05+01:00"))
+            .to(OffsetDateTime.parse("2023-10-02T19:10+01:00"))
+            .exec();
+        WaterLevelMeasurement m1 = iter.next();
+        assertEquals("Vandstand", m1.parameter());
+        WaterLevelMeasurement m2 = iter.next();
+        assertEquals("cm", m2.unit());
+        assertThrows(NoSuchElementException.class, iter::next);
     }
 }
